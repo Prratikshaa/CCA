2e75d4c0f7af3cfab3ec1e61b0d093525c18232d
Pratiksha Chavan <pratikshachavan1002@gmail.com>
2024-11-15 21:21:21 +0530

    HOST

--- CCA/settings.py
+++ CCA/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-y_)aqu*v&uxvtdj^1h*^l@uabl9u-7wpn!n@vg%t&2(gh1hmuy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ 'cca-6u5j.onrender.com','127.0.0.1']
+ALLOWED_HOSTS = [ "https://cca-6u5j.onrender.com",'127.0.0.1']
 
 
 # Application definition
